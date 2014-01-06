ad54ab7c15014916a18278fe57b527981ec3aa9b
tyraela <wow@codeart.ch>
2014-01-06 02:59:07 +0100

    GM hover command sql improvement
    Changed to .sql fileextension and added delete statement

--- sql/updates/sxxxx_01_mangos_command.sql
+++ sql/updates/sxxxx_01_mangos_command.sql
@@ -1,4 +1,7 @@
 -- New .hover on/off command
+DELETE FROM `command` WHERE `name` = 'hover';
+DELETE FROM `mangos_string` WHERE `entry` = '555';
+DELETE FROM `mangos_string` WHERE `entry` = '556';
 INSERT INTO `command` (`name`, `security`, `help`) VALUES ('hover', '2', 'Syntax: .hover on/off\r\n\r\nSet on/off hover state for selected player.');
 INSERT INTO `mangos_string` (`entry`, `content_default`) VALUES ('555', 'Hover enabled');
 INSERT INTO `mangos_string` (`entry`, `content_default`) VALUES ('556', 'Hover disabled');
