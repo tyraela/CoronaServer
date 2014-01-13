615ba414afc32678a0b1027657501ca03f1eac1f
tyraela <wow@codeart.ch>
2014-01-14 00:25:42 +0100

    Fix movement lag from speed rate changes
    -Fixed movement lag caused by speed rate changes. Example: Fear (first 2
    sec)

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -7358,6 +7358,9 @@ bool Unit::canDetectInvisibilityOf(Unit const* u) const
 
 void Unit::UpdateSpeed(UnitMoveType mtype, bool forced, float ratio)
 {
+    //if (isFeared())
+    //    return;
+    
     // not in combat pet have same speed as owner
     switch (mtype)
     {
@@ -7513,47 +7516,50 @@ void Unit::SetSpeedRate(UnitMoveType mtype, float rate, bool forced)
         rate = 0.0f;
 
     // Update speed only on change
-    if (m_speed_rate[mtype] != rate)
+    if (m_speed_rate[mtype] == rate)
+        return;
+    
+    const uint16 SetSpeed2Opc_table[MAX_MOVE_TYPE][2] =
     {
-        m_speed_rate[mtype] = rate;
-        propagateSpeedChange();
+        {MSG_MOVE_SET_WALK_SPEED,       SMSG_FORCE_WALK_SPEED_CHANGE},
+        {MSG_MOVE_SET_RUN_SPEED,        SMSG_FORCE_RUN_SPEED_CHANGE},
+        {MSG_MOVE_SET_RUN_BACK_SPEED,   SMSG_FORCE_RUN_BACK_SPEED_CHANGE},
+        {MSG_MOVE_SET_SWIM_SPEED,       SMSG_FORCE_SWIM_SPEED_CHANGE},
+        {MSG_MOVE_SET_SWIM_BACK_SPEED,  SMSG_FORCE_SWIM_BACK_SPEED_CHANGE},
+        {MSG_MOVE_SET_TURN_RATE,        SMSG_FORCE_TURN_RATE_CHANGE},
+        {MSG_MOVE_SET_FLIGHT_SPEED,     SMSG_FORCE_FLIGHT_SPEED_CHANGE},
+        {MSG_MOVE_SET_FLIGHT_BACK_SPEED, SMSG_FORCE_FLIGHT_BACK_SPEED_CHANGE},
+    };
 
-        const uint16 SetSpeed2Opc_table[MAX_MOVE_TYPE][2] =
-        {
-            {MSG_MOVE_SET_WALK_SPEED,       SMSG_FORCE_WALK_SPEED_CHANGE},
-            {MSG_MOVE_SET_RUN_SPEED,        SMSG_FORCE_RUN_SPEED_CHANGE},
-            {MSG_MOVE_SET_RUN_BACK_SPEED,   SMSG_FORCE_RUN_BACK_SPEED_CHANGE},
-            {MSG_MOVE_SET_SWIM_SPEED,       SMSG_FORCE_SWIM_SPEED_CHANGE},
-            {MSG_MOVE_SET_SWIM_BACK_SPEED,  SMSG_FORCE_SWIM_BACK_SPEED_CHANGE},
-            {MSG_MOVE_SET_TURN_RATE,        SMSG_FORCE_TURN_RATE_CHANGE},
-            {MSG_MOVE_SET_FLIGHT_SPEED,     SMSG_FORCE_FLIGHT_SPEED_CHANGE},
-            {MSG_MOVE_SET_FLIGHT_BACK_SPEED, SMSG_FORCE_FLIGHT_BACK_SPEED_CHANGE},
-        };
+    m_speed_rate[mtype] = rate;
+    propagateSpeedChange();
 
-        if (forced && GetTypeId() == TYPEID_PLAYER)
+    // m_movementInfo.UpdateTime(WorldTimer::getMSTime());
+
+    if (forced)
+    {
+        if (GetTypeId() == TYPEID_PLAYER)
         {
             // register forced speed changes for WorldSession::HandleForceSpeedChangeAck
             // and do it only for real sent packets and use run for run/mounted as client expected
             ++((Player*)this)->m_forced_speed_changes[mtype];
-
-            WorldPacket data(Opcodes(SetSpeed2Opc_table[mtype][1]), 18);
-            data << GetPackGUID();
-            data << (uint32)0;                              // moveEvent, NUM_PMOVE_EVTS = 0x39
-            if (mtype == MOVE_RUN)
-                data << uint8(0);                           // new 2.1.0
-            data << float(GetSpeed(mtype));
-
-            ((Player*)this)->GetSession()->SendPacket(&data);
         }
 
-        m_movementInfo.UpdateTime(WorldTimer::getMSTime());
-
-        // TODO: Actually such opcodes should (always?) be packed with SMSG_COMPRESSED_MOVES
-        WorldPacket data(Opcodes(SetSpeed2Opc_table[mtype][0]), 64);
+        WorldPacket data(Opcodes(SetSpeed2Opc_table[mtype][1]), 18);
+        data << GetPackGUID();
+        data << (uint32)0;                              // moveEvent, NUM_PMOVE_EVTS = 0x39
+        if (mtype == MOVE_RUN)
+            data << uint8(0);                           // new 2.1.0
+        data << float(GetSpeed(mtype));
+        SendMessageToSet(&data, true);
+    }
+    else
+    {
+        WorldPacket data(Opcodes(SetSpeed2Opc_table[mtype][0]), 42);
         data << GetPackGUID();
         data << m_movementInfo;
         data << float(GetSpeed(mtype));
-        SendMessageToSet(&data, false);
+        SendMessageToSet(&data, true);
     }
 
     CallForAllControlledUnits(SetSpeedRateHelper(mtype, forced), CONTROLLED_PET | CONTROLLED_GUARDIANS | CONTROLLED_CHARM | CONTROLLED_MINIPET);
