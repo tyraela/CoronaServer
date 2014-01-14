e90f2547a6ccbd8ad6cced4d3b3718b96ac4e94c
Salja <salja8840@gmail.com>
2014-01-14 19:36:04 +0100

    Fix Stuns in Flight *temp Fix (amazing testcase by tyraela)
    Signed-off-by: Salja <salja8840@gmail.com>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -3616,6 +3616,9 @@ void Aura::HandleAuraModDisarm(bool apply, bool Real)
 
 void Aura::HandleAuraModStun(bool apply, bool Real)
 {
+    if (GetTarget()->IsTaxiFlying())
+        return;
+
     if (!Real)
         return;
 
